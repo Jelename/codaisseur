2f5e5f5379e4314cb784d9691f8690219ffc2ace
Jelename <yelena.medenica@gmail.com>
2018-05-31 14:27:50 +0200

    make todo list, add items to that list, iterate over that array

---
+++ to-do-app/js.js
@@ -0,0 +1,10 @@
+let toDoList = [
+  'Write users stories',
+  'break first feature down in to steps',
+  'make first feature',
+  'make todo app'
+];
+
+toDoList.forEach(function(task) {
+  console.log(task);
+})
