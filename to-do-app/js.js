8f51ac620ea65381976ee99cf1ca105ddf2f5627
Jelename <yelena.medenica@gmail.com>
2018-06-01 09:45:21 +0200

    removing elements

--- to-do-app/js.js
+++ to-do-app/js.js
@@ -13,12 +13,30 @@ function takeInput() {
 function updateList(itemNew) {
     let listItem = document.createElement('li');
     let todoText = document.createTextNode(itemNew);
+    let checkboxItem = document.createElement('input');
+    checkboxItem.setAttribute('type', 'checkbox');
+    checkboxItem.setAttribute('class', 'checkBox');
+    listItem.appendChild(checkboxItem);
     listItem.appendChild(todoText);
     todoList.appendChild(listItem);
     addToList(itemNew);
   }
 
-  function addToList(itemNew) {
-    todoItems.push(itemNew);
-    console.log(todoItems);
-  }
+
+function addToList(itemNew) {
+  todoItems.push(itemNew);
+  removeFromList();
+}
+
+// function removeFromList() {
+//   let finishedItems = todoItems.filter(removingItems);
+// }
+
+function removeFromList() {
+  let itemsToRemove = document.getElementsByTagName('li');
+  itemsToRemove.addEventListener('click', clickme(this));
+}
+
+// function clickme(htmlElement) {
+//   console.log(htmlElement.innerHTML = "DONE");
+// }
